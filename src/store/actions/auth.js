f034e351f9bf4e45ac2bfaa59d004bd01e388c3d
Christopher Fiallos <Athroscf@users.noreply.github.com>
2020-07-24 21:17:53 -0600

    'Not showing email on welcome' bug fixed.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,11 +9,12 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = ( token, userId ) => {
+export const authSuccess = ( token, userId, email ) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
         idToken: token,
-        userId: userId
+        userId: userId,
+        email: email
     };
 };
 
